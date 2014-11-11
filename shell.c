4e80ae399f4cfeb0856bc43c33028c20603c47bd
Justin George <george.justin16@me.com>
2014-11-11 18:45:03 -0400

    Added the string tokenizer, but there is still an issue, if someone can figure it out

--- shell.c
+++ shell.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char* argv[], char* envp[])
 {
-	char c = '\0';
+	char c[] = 256;
+        char *ch;
 	printf("Welcome to the NeverWinter Shell!");
 	printf("\n[MY_SHELL  ]:- ");
 	while (c!= EOF)
 	{
 		c = getchar();
 		//Store every word as an array of charachters to parse later
-
+		ch = strtok(ch, " ");
+		while (ch != NULL)
+		{
+			ch = strtok(NULL, " ,");
+		}
 		//Once the words are stored, we can start the fork, exec and wait
 		int rc = fork();
 
@@ -23,7 +29,7 @@ int main(int argc, char* argv[], char* envp[])
 
 		else if(rc == 0)	// Now we can execute the command that we get from the array of charachters that we stored
 		{
-			printf("Executing command: ");
+			printf("Executing command: child ");
 		}
 		
 		else if(rc > 0)		//Show wait() and then run MY_SHELL again
