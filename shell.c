b0e4786ff6e76602cf91e686c98db1c5773c4b9b
rikkustrife <danicom23@yahoo.ca>
2014-11-25 23:29:48 +0000

    Added code for piping, not working yet.

--- shell.c
+++ shell.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <fcntl.h>
 
 #define COLOR_BLUE	"\x1b[34m"
 #define COLOR_RESET	"\x1b[0m" 
@@ -19,7 +20,7 @@ int main(int argc, char* argv[], char* envp[])
 	printf(COLOR_RESET);
 	bool isPiping = false, isRedirecting = false, isWildcard = false;
 	char *pipeSearch, *redirectionSearch, *wildcardSearch;
-	int  pipingPosition[256], redirectionPosition[256], wildcardPosition[256];
+	int  pipingPosition[256] = { 0 }, redirectionPosition[256] = { 0 }, wildcardPosition[256] = { 0 };
 	char c[256] = "\0";
 	char* parameters[256];
 	char *cptr;
@@ -59,35 +60,7 @@ int main(int argc, char* argv[], char* envp[])
 			parameterCount++;
 			cptr = strtok(NULL, " ");	
 	 	}
-		int i, j = 0, k = 0, m = 0;
-		for (i = 0; i < parameterCount; i++)
-		{
-			pipeSearch = strchr(parameters[i],piping);
-			if (pipeSearch!= NULL)
-			{
-				pipingPosition[j] = i;
-				printf("| in position %d\n", pipingPosition[j]);
-				isPiping = true;
-				j++;
-			}
-			redirectionSearch = strchr(parameters[i],redirection);
-			if (redirectionSearch!= NULL)
-			{
-				redirectionPosition[k] = i;
-				printf("> in position %d\n", redirectionPosition[k]);
-				isRedirecting = true;
-				k++;
-			}
-			wildcardSearch = strchr(parameters[i],wildcard);
-			if (wildcardSearch!= NULL)
-			{	
-				wildcardPosition[m] = i;
-				printf("* in position %d\n", wildcardPosition[m]);
-				isWildcard = true;
-				m++;
-			}
 
-		}
 		//Exiting out of the shell		
 		int ret;
 		ret = strcmp(parameters[0],"exit");
@@ -113,27 +86,72 @@ int main(int argc, char* argv[], char* envp[])
                         strcpy(prog, path);
                         //Then we concancate the program name to /bin/
                         //If the program name is ls, then it'll be /bin/ls
-                        strcat(prog, parameters[0]);
+		int i, j = 0, k = 0, m = 0;
+		for (i = 0; i < parameterCount; i++)
+		{
+			pipeSearch = strchr(parameters[i],piping);
+			redirectionSearch = strchr(parameters[i],redirection);
+			if (pipeSearch!= NULL)
+			{
+				int command[2];
+				pipingPosition[j] = i;
+				strcat(prog, parameters[pipingPosition[j]-1]);
+				printf("| in position %d\n", pipingPosition[j]);
+				dup2(command[1],1);
+				execve(prog,parameters,envList);
+				strcpy(prog, path);
+				strcpy(prog, path);
+				strcat(prog, parameters[pipingPosition[j]+1]);
+				dup2(command[0],0);
+				execve(prog,parameters,envList);
+				j++;
+			}
+
+			else if (redirectionSearch!= NULL)
+			{
+				redirectionPosition[k] = i;
+				printf("> in position %d\n", redirectionPosition[k]);
+				isRedirecting = true;
+				k++;
+			}
+			else
+			{
+		                strcat(prog, parameters[i]);
+				int retc = execve(prog,parameters,envList);
+				if (retc == -1)
+				{
+					strcpy(prog,"");
+					strcat(prog,parameters[i]);
+					retc = execve(prog,parameters,envList);
+					if(retc == -1)
+					printf("The process %s could not be recognized.\n",c);
+				}
+			}	
+		/*	wildcardSearch = strchr(parameters[i],wildcard);
+			if (wildcardSearch!= NULL)
+			{	
+				wildcardPosition[m] = i;
+				printf("* in position %d\n", wildcardPosition[m]);
+				isWildcard = true;
+				m++;
+			}*/
+
+		}
+
 			if (isPiping == true)
 			{
-				//Piping code
+//								while 
 			}
 			if (isRedirecting == true)
 			{
 				//Redirection code
 			}
 			//Then execute the damn progra
-			int retc = execve(prog,parameters,envList);
-			if (retc == -1)
-			{
-				strcpy(prog,"");
-				strcat(prog,parameters[0]);
-				retc = execve(prog,parameters,envList);
-				if(retc == -1)
-				printf("The process %s could not be recognized.\n",c);
+			/*
+
 				return 0;
 			}
-		
+		*/
 			return 0;
 		}
 
