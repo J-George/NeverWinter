868689ee715e0b6f7466cb97890d67a8d6bb8282
Justin George <george.justin16@me.com>
2014-11-26 15:05:09 -0400

    Added /usr/bin/

--- shell.c
+++ shell.c
@@ -9,7 +9,7 @@
 #include <stdbool.h>
 #include <fcntl.h>
 
-static char *line_read = (char *)NULL;
+//static char *line_read = (char *)NULL;
 
 #define COLOR_BLUE	"\x1b[34m"
 #define COLOR_RESET	"\x1b[0m" 
@@ -32,19 +32,19 @@ int main(int argc, char* argv[], char* envp[])
 
 
 	char* c;
-	char* parameters[256];
+	char* parameters[512];
 	char *cptr;
 	int parameterCount = 0;
 
 	
-	char piping = '|', redirection = '>', wildcard  ='*';
+	char piping = '|', redirection = '>'; //, wildcard  ='*';
 
         while(1)
 	{
 		int z;
  		char* path = "/bin/";
 		char* envList[] = {"HOME=/root", "PATH=/bin:/sbin", NULL};
-
+	
 		//Loop that resets arrays to null
 		for (z=0; z<= sizeof(c); z++)
 		{
@@ -59,13 +59,13 @@ int main(int argc, char* argv[], char* envp[])
 		reg = strcmp(c,"");
 		while (reg == 0)
 		{
-			c = readline(COLOR_BLUE"[BULE_SHELL  ]:- "COLOR_RESET);
+			c = readline(COLOR_BLUE"[BLUE_SHELL  ]:- "COLOR_RESET);
 			reg = strcmp(c,"");
 			if (reg != 0)
 			break;
 		}
 		add_history(c);
-
+	
         	//Store every word as an array of strings to parse later
 		cptr = strtok(c, " ");
 		while (cptr != NULL)
@@ -75,10 +75,7 @@ int main(int argc, char* argv[], char* envp[])
 			cptr = strtok(NULL, " ");	
 	 	}
 
-
-
-
-
+	
 		//debugging.. set to 1 to see all the arguments on each seperate line
 		if(0)
 		{	
@@ -95,7 +92,7 @@ int main(int argc, char* argv[], char* envp[])
 		ret = strcmp(parameters[0],"exit");
 		if(ret == 0)
 		exit(0);
-
+	
 		//Fork
 		int rc = fork();
 		//if Fork fails
@@ -108,86 +105,94 @@ int main(int argc, char* argv[], char* envp[])
 		//Child Code
 		else if(rc == 0) 
 		{
-			//printf("Child Runs\n");
 			//This will be the final path to the program that we will pass to execv
                         char prog[1024];
                         //First we copy a /bin/ to prog
                         strcpy(prog, path);
 
-                //Then we concancate the program name to /bin/
-                //If the program name is ls, then it'll be /bin/ls
-
-		// Allowing for detection of piping, redirection and wildcard implementation
-
-		int i, j = 0, k = 0, m = 0;
-		for (i = 0; i < parameterCount; i++)
-		{
-			pipeSearch = strchr(parameters[i],piping);
-			redirectionSearch = strchr(parameters[i],redirection);
-			if (pipeSearch!= NULL)
+			// Allowing for detection of piping, redirection and wildcard implementation
+			//Piping
+			int i, j = 0, k = 0, m = 0;
+			for (i = 0; i < parameterCount; i++)
 			{
-				int command[2];
-				pipingPosition[j] = i;
-				strcat(prog, parameters[pipingPosition[j]-1]);
-				printf("| in position %d\n", pipingPosition[j]);
-				dup2(command[1],1);
-				execve(prog,parameters,envList);
-				strcpy(prog, path);
-				strcpy(prog, path);
-				strcat(prog, parameters[pipingPosition[j]+1]);
-				dup2(command[0],0);
-				execve(prog,parameters,envList);
-				j++;
-			}
-
-			else if (redirectionSearch!= NULL)
-			{
-				redirectionPosition[k] = i;
-				printf("> in position %d\n", redirectionPosition[k]);
-				isRedirecting = true;
-				k++;
-			}
-			else
-			{
-		                strcat(prog, parameters[i]);
-				int retc = execve(prog,parameters,envList);
-				if (retc == -1)
+				pipeSearch = strchr(parameters[i],piping);
+				redirectionSearch = strchr(parameters[i],redirection);
+				if (pipeSearch!= NULL)
 				{
-					strcpy(prog,"");
-					strcat(prog,parameters[i]);
-					retc = execve(prog,parameters,envList);
-					if(retc == -1)
-					printf("The process %s could not be recognized.\n",c);
+					int command[2];
+					pipingPosition[j] = i;
+					strcat(prog, parameters[pipingPosition[j]-1]);
+					printf("| in position %d\n", pipingPosition[j]);
+					dup2(command[1],1);
+					execve(prog,parameters,envList);
+					strcpy(prog, path);
+					strcpy(prog, path);
+					strcat(prog, parameters[pipingPosition[j]+1]);
+					dup2(command[0],0);
+					execve(prog,parameters,envList);
+					j++;
 				}
-			}	
-		/*	wildcardSearch = strchr(parameters[i],wildcard);
-			if (wildcardSearch!= NULL)
-			{	
-				wildcardPosition[m] = i;
-				printf("* in position %d\n", wildcardPosition[m]);
-				isWildcard = true;
-				m++;
-			}*/
+				// Redirection code
+				else if (redirectionSearch!= NULL)
+				{
+					redirectionPosition[k] = i;
+					printf("> in position %d\n", redirectionPosition[k]);
+					isRedirecting = true;
+					k++;
+				}
+				//Execute the program from bash, or by itself
+				else
+				{
+					//Then we concancate the program name to /bin/
+		                	strcat(prog, parameters[i]);
+					//If the program name is ls, then it'll be /bin/ls
+					int retc = execve(prog,parameters,envList);
+					if (retc == -1)
+					{
+						strcpy(prog,"");
+						strcat(prog,parameters[i]);
+						retc = execve(prog,parameters,envList);
+						if (retc == -1)
+						{
+							strcpy(prog,"/usr/bin/");
+							//printf("Checked /usr/bin\n");
+							strcat(prog,parameters[i]);
+							retc = execv(prog,parameters);
+							if(retc == -1)
+							{
+								printf("The process %s could not be recognized.\n",c);
+							}
+					}
+				}	
+		                /*wildcardSearch = strchr(parameters[i],wildcard);
+				if (wildcardSearch!= NULL)
+				{	
+					wildcardPosition[m] = i;
+					printf("* in position %d\n", wildcardPosition[m]);
+					isWildcard = true;
+					m++;
+				}*/
 
-		}
-
-			if (isPiping == true)
-			{
-//								while 
 			}
-			if (isRedirecting == true)
-			{
-				//Redirection code
-			}
-			//Then execute the damn progra
-			return 0;
-		}
 
+				if (isPiping == true)
+				{
+                                  //while
+				 
+				}
+				if (isRedirecting == true)
+				{
+				  //Redirection code
+				}
+			
+				return 0;
+			}
+		}
 		//Parent Code
 		else if(rc > 0)	
 		{
 			wait(NULL);	
 		}
-		
+			
 	}
 }
