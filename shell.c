41a8077420483bf624316f6005ceaf95eadef612
Justin George <george.justin16@me.com>
2014-11-27 14:21:15 -0400

    For my branch

--- shell.c
+++ shell.c
@@ -1,97 +1,20 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include <sys/wait.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <stdbool.h>
 #include <fcntl.h>
 
+
 //static char *line_read = (char *)NULL;
 
+#define COLOR_RED	"\x1b[35m"
 #define COLOR_BLUE	"\x1b[34m"
 #define COLOR_RESET	"\x1b[0m" 
-/* the current commented out code in regards to piping is a failed attempt to
-read from input command, unsure how to fix, if fixed these next 2 lines 
-can be commented, currently it pipes command1 into command2, does not return to shell after piping, it seems command2 doesn't close 
-*/
-char *command1[] = { "/bin/ls", "-a", "/", 0 };
-char *command2[] = { "/usr/bin/tr", "a-z", "A-Z", 0 };
-
-
-void pipeIn(int command[]/*, char* command1[], char* parameters*/)
-{
-	int pid = fork();
-	if (pid  == 0) //child
-	{
-		dup2(command[1],1);
-		close(command[0]);
-		int ret = execvp(command1[0], command1);
-		if (ret = -1)
-		{
-		/*	char* otherPath = "/usr/bin";
-			strcat(otherPath, parameters);
-			strcpy(commands[0], otherPath);
-			ret = execvp(commands[0], commands);
-			if (ret = -1)
-			{*/
-				printf("Path not found");
-				return;
-		//	}
-		}	
-		close(1);
-	}
-	if (pid < 0)
-	{
-		printf("fork error");
-		exit(1);
-	}
-	if (pid > 0)
-	{
-		wait(NULL);
-	}
-}
-
-void pipeOut(int command[]/*, char* command2[], char* parameters*/) 
-{
-	int pid = fork();
-	if (pid == 0) //child
-	{
-		dup2(command[0],0);
-		close(command[1]);
-		int ret = execvp(command2[0], command2);
-		if (ret = -1)
-		{
-		/*	char* otherPath = "/usr/bin";
-			strcat(otherPath, parameters);
-			strcpy(commands[0], otherPath);
-			ret = execvp(commands[0], commands);
-			if (ret = -1)
-			{ */
-				printf("Path not found");
-				return;
-		//	}
-		}
-		close(0);
-		close(1);
-		close(command[0]);
-		close(command[1]);	
-}
-	if (pid < 0)
-        {
-                printf("fork error");
-                exit(1);
-        }
-        if (pid > 0)
-        {
-                wait(NULL);
-		close(command[0]);
-		close(command[1]);
-        }
-}
 
 int main(int argc, char* argv[], char* envp[])
 {
@@ -100,43 +23,58 @@ int main(int argc, char* argv[], char* envp[])
 	printf(" ||    ||  ||       ||   ||  ||          ||        ||    ||  ||      ||      ||\n");
 	printf(" ||----    ||       ||   ||  ||=====     \\\\----\\\\  ||====||  ||====  ||      ||\n");
 	printf(" ||    ||  ||       ||   ||  ||                ||  ||    ||  ||      ||      ||\n");
-	printf(" ||____/   ||=====  \\\\---//  ||=====     ======//  ||    ||  ||====  ||====  ||====\n\n");
+	printf(" ||____/   ||=====  \\\\---//  ||=====     ======//  ||    ||  ||====  ||====  ||====\n");
 	printf(COLOR_RESET);
 
-
-	bool isPiping = false, isRedirecting = false, isWildcard = false;
-	char *pipeSearch, *redirectionSearch, *wildcardSearch;
-	int  pipingPosition[256] = { 0 }, redirectionPosition[256] = { 0 }, wildcardPosition[256] = { 0 };
-
-
 	char* c;
-	char* parameters[512];
+	char* parameters[128];
 	char *cptr;
 	int parameterCount = 0;
-
 	
-	char piping = '|', redirection = '>'; //, wildcard  ='*';
+	//char piping = '|', redirection = '>', wildcard  ='*';
 
+	//Little code for having the time displayed as well as the shell name
+	time_t rawtime;
+	struct tm * timeinfo;
+	time (&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	printf("\nLog-in time: %s",asctime(timeinfo));
+	char* pegpoint = NULL;
+	
         while(1)
 	{
 		int z;
  		char* path = "/bin/";
 		char* envList[] = {"HOME=/root", "PATH=/bin:/sbin", NULL};
-	
-		//Loop that resets arrays to null
-		for (z=0; z<= sizeof(c); z++)
+		bool chngdir = false;
+		char *change = NULL;
+
+		//Loop that resets arrays to null			
+		for (z=0; z<= 128; z++)
 		{
-			c[z] = '\0';
 			parameters[z] = NULL;
 		}
 		parameterCount = 0;
-		
+
 		//Display and read the message
 		int reg;
-		c = readline(COLOR_BLUE"[BLUE_SHELL  ]:- "COLOR_RESET);
+		//Current working directory display
+		char cwd[1024];
+		getcwd(cwd,sizeof(cwd));
+		printf(COLOR_BLUE"\n(%s)"COLOR_RESET,cwd);
+		
+		if(pegpoint!=NULL)
+		{
+			printf(COLOR_RED"\t\t\t Peg:(%s)"COLOR_RESET,pegpoint);
+		}
+
+		//prompt display
+		c = readline(COLOR_BLUE"\n[BLUE_SHELL  ]:- "COLOR_RESET);
 		reg = strcmp(c,"");
 		while (reg == 0)
-		{
+		{	
+			printf(COLOR_BLUE"\n(%s)\n"COLOR_RESET,cwd);
 			c = readline(COLOR_BLUE"[BLUE_SHELL  ]:- "COLOR_RESET);
 			reg = strcmp(c,"");
 			if (reg != 0)
@@ -160,7 +98,7 @@ int main(int argc, char* argv[], char* envp[])
 			int s = 0;
 			while(s != parameterCount)
 			{
-				printf("%s\n",parameters[s]);
+				printf("%d: %s\n",s,parameters[s]);
 				s++;
 			}		
 		}
@@ -169,7 +107,56 @@ int main(int argc, char* argv[], char* envp[])
 		int ret;
 		ret = strcmp(parameters[0],"exit");
 		if(ret == 0)
-		exit(0);
+		{
+			printf("\nLog-out time: %s\n",asctime(timeinfo));
+			exit(0);
+		}
+
+		//Special feature: Pegging
+		int pegVal;
+		pegVal = strcmp(parameters[0],"peg");
+		if(pegVal == 0)
+		{
+			char dir[1024];
+			pegpoint = getcwd(dir,sizeof(dir));
+			continue;
+		}
+		pegVal = strcmp(parameters[0],"rmpeg");
+		if(pegVal == 0)
+		{
+			if(pegpoint == NULL)
+			{
+				printf("No peg in place to remove. Type \"peg\" to place holder.\n");
+				continue;
+			}
+			pegpoint = NULL;
+			continue;
+		}
+		pegVal = strcmp(parameters[0],"mv2peg");
+		if(pegVal ==0)
+		{
+			//printf("%s\n",pegpoint);
+			if(pegpoint == NULL)
+			{
+				printf("No peg set in place to move to. Type \"peg\" to place holder.\n");
+				continue;
+			}
+			int s;
+			int counter = 0;
+			int check;
+			for(s=0;s<=strlen(pegpoint);s++)
+				{
+					printf("%c",pegpoint[s]);
+					check = strncmp(&pegpoint[s],"/",1);
+					if(check ==0)
+						counter++; 	
+				}
+			printf("\nTotal /'s: %d\n",counter);
+			char mover[128];
+			strcat(mover,"../..");
+			continue;
+		}
+
 	
 		//Fork
 		int rc = fork();
@@ -185,50 +172,33 @@ int main(int argc, char* argv[], char* envp[])
 		{
 			//This will be the final path to the program that we will pass to execv
                         char prog[1024];
+
                         //First we copy a /bin/ to prog
                         strcpy(prog, path);
 
-			// Allowing for detection of piping, redirection and wildcard implementation
-			//Piping
-			int i, j = 0, k = 0, m = 0;
+			// Allowing for detection of piping, redirection
+			int i;
 			for (i = 0; i < parameterCount; i++)
-			{
-				pipeSearch = strchr(parameters[i],piping);
-				redirectionSearch = strchr(parameters[i],redirection);
-				if (pipeSearch!= NULL)
-				{
-						//pipingPosition[j] = i;
-			/*        char* command1[5];// command[0] is the command, everyone after should be the parameters of that command, such -a for ls, or for tr(translate) it would be what you want to translate to, like change to all caps, getting this is the issue  
-				char* command2[5];
-				command1[0] = path;
-				command2[0] = path;
-				strcat(command1[0], parameters[pipingPosition[j]-1]);
-			        strcat(command2[0], parameters[pipingPosition[j]+1]);
-				//isPiping = true;
-			*/
-				int command[2];
-				pipe(command);
-				pipeIn(command/* , command1, parameters[pipingPosition[j]-1]*/);
-				pipeOut(command/*, command2, parameters[pipingPosition[j]+1]*/);
-				close(command[0]);
-				close(command[1]);
-				close(0);
-				close(1);
-				}
-				// Redirection code
-				else if (redirectionSearch!= NULL)
-				{
-					redirectionPosition[k] = i;
-					printf("> in position %d\n", redirectionPosition[k]);
-					isRedirecting = true;
-					k++;
-				}
+			{	
 				//Execute the program from bash, or by itself
-				else
-				{
-					//Then we concancate the program name to /bin/
-		                	strcat(prog, parameters[i]);
-					//If the program name is ls, then it'll be /bin/ls
+				//Then we concancate the program name to /bin/
+				//If the program name is ls, then it'll be /bin/ls
+		                	
+					//Special Case for cd
+					int cd = strcmp(parameters[i],"cd");
+					if(cd == 0)
+					{	
+						change = parameters[1];
+						cd = chdir(change);
+						if(cd == 0)
+							chngdir = true;
+						else
+							printf("%s:No such file or directory found!\n",parameters[1]); 	
+						return 0;
+					}
+					
+					//For other commands follow the path
+					strcat(prog, parameters[i]);
 					int retc = execve(prog,parameters,envList);
 					if (retc == -1)
 					{
@@ -241,40 +211,29 @@ int main(int argc, char* argv[], char* envp[])
 							//printf("Checked /usr/bin\n");
 							strcat(prog,parameters[i]);
 							retc = execv(prog,parameters);
-							if(retc == -1)
+							if (retc == -1)
 							{
-								printf("The process %s could not be recognized.\n",c);
-							}
-					}
-				}	
-		                /*wildcardSearch = strchr(parameters[i],wildcard);
-				if (wildcardSearch!= NULL)
-				{	
-					wildcardPosition[m] = i;
-					printf("* in position %d\n", wildcardPosition[m]);
-					isWildcard = true;
-					m++;
-				}*/
-
-			}
-
-				if (isPiping == true)
-				{
-                                  //while
-				 
-				}
-				if (isRedirecting == true)
-				{
-				  //Redirection code
-				}
-			
-				return 0;
+								strcpy(prog,"/sbin/");
+								strcat(prog,parameters[i]);
+								retc = execv(prog,parameters);
+								if(retc == -1)
+								{
+									printf("The process %s could not be recognized.\n",c);
+								}
+							 }
+				         	}
+				}		
 			}
 		}
+
 		//Parent Code
 		else if(rc > 0)	
 		{
-			wait(NULL);	
+			wait(NULL);
+			//printf("Parent finished waiting\n");
+			change = parameters[1];
+			if(chngdir == true);
+				chdir(change);	
 		}
 			
 	}
